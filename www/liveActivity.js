c3641e3a75fdd7d7acd1dc16ae348758b2d89156
ibstrv <ivan@bstrv.ru>
2025-01-12 00:35:45 +0300

    Update liveActivity.js

--- www/liveActivity.js
+++ www/liveActivity.js
@@ -1,14 +1,11 @@
-cordova.define("cordova-plugin-liveactivity.liveactivity", function(require, exports, module) {
+LiveActivity = function() {};
+    
+LiveActivity.prototype.start = function(message, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'LiveActivity', 'start', [message]);
+};
 
-    LiveActivity = function() {};
+LiveActivity.prototype.getStartToken = function(message, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'LiveActivity', 'start', [message]);
+};
     
-    LiveActivity.prototype.start = function(message, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, 'LiveActivity', 'start', [message]);
-    };
-    
-    LiveActivity.prototype.getStartToken = function(message, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, 'LiveActivity', 'start', [message]);
-    };
-    
-    module.exports = new LiveActivity();
-});
+module.exports = new LiveActivity();
